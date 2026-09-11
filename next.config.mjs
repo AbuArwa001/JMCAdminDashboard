f2cb597c50eeb0884798b5bf24492147c4443d79
AbuArwa001 <khalfanathman12@yahoo.com>
2026-09-11 23:27:02 +0300

    Add donations-api.jamiamosque.co.ke to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -41,6 +41,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'donations-api.jamiamosque.co.ke',
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
     eslint: {
